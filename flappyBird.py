b3363409e2fabed7ee5c8fd7d8d07493d69be904
amer <149695414+aboObaidah@users.noreply.github.com>
2023-11-11 20:43:15 +0200

    Update flappyBird.py

--- flappyBird.py
+++ flappyBird.py
@@ -1,10 +1,7 @@
 from ursina import *
 import random as r
-
-
 app = Ursina()
 Sky()
-
 bird = Animation('assets\img',
                  collider='box',
                  scale=(2,2,2),
@@ -12,7 +9,6 @@ bird = Animation('assets\img',
 
 camera.orthographic = True
 camera.fov = 20
-
 def update():
   bird.y = bird.y - 4*time.dt
   for p in pipes:
@@ -20,28 +16,21 @@ def update():
   touch = bird.intersects()
   if touch.hit or bird.y < -10:
     quit()
-
-
 def input(key):
   if key == 'space':
     bird.y = bird.y + 3
-
 pipes = []
-
 pipe = Entity(model='quad',
               color=color.green,
               texture='white_cube',
               position=(20,10),
               scale=(3,15,1),
               collider='box')
-
 def newPipe():
   y = r.randint(4, 12)
   new1 = duplicate(pipe, y=y)
   new2 = duplicate(pipe, y=y-22)
   pipes.extend((new1, new2))
   invoke(newPipe, delay=5)
-
 newPipe()
-
 app.run()
